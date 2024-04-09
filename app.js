e178a8f2bbf207b8f9d1a7460365e5d480851b07
Elhama <tokhielhama30@gmail.com>
2024-04-09 12:09:08 +0200

    Sat enviroment variables

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+import "dotenv/config";
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-
 mongoose
-  .connect("mongodb://127.0.0.1:27017/NextBook")
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("Database Connected"))
   .catch((error) => console.error(error));
 
